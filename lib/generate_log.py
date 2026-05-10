6d76a95aebfd42c0f9d0f2c3d655de05d6220e62
Z-Link-C <zachery.croft@gmail.com>
2026-05-10 01:46:24 -0400

    fixed missing returns and checks

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -4,26 +4,17 @@ import requests
 
 def generate_log(data):
     # TODO: Implement log generation logic
-
-    # STEP 1: Validate input
-    # Hint: Check if data is a list
-    log_data=["User logged in","User updated profile","Report exported"]
-    filename=f"log_{datetime.now().strftime('%Y%m%d')}"
+    if not isinstance(data,list):
+        raise ValueError("This should be a list")
+    log_data=data
+    filename=(f"log_{datetime.now().strftime("%Y%m%d")}.txt")
+    #filename=(f"log_{datetime.now().strftime('%Y%m%d')}.txt")
 
     with open(filename,"w") as file:
         for e in log_data:
             file.write(f"{e}\n")
     print(f"Log written to {filename}")
-
-    # STEP 2: Generate a filename with today's date (e.g., "log_20250408.txt")
-    # Hint: Use datetime.now().strftime("%Y%m%d")
-
-    # STEP 3: Write the log entries to a file using File I/O
-    # Use a with open() block and write each line from the data list
-    # Example: file.write(f"{entry}\n")
-
-    # STEP 4: Print a confirmation message with the filename
-
+    return filename
 def fetch_data():
     response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
     if response.status_code == 200:
